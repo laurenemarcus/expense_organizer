51b3c5fb938138e94fb5b1a6283f63dbc990eb94
Lauren Marcus & Arsi <student@epicodus.com>
2015-01-22 11:24:46 -0800

    Allow users to enter expenses into a database.

--- spec/expense_organizer_spec.rb
+++ spec/expense_organizer_spec.rb
@@ -2,11 +2,11 @@ require("rspec")
 require("expense_organizer")
 require("pg")
 
-DB = PG.connect({:dbname => expense_organizer_test})
+DB = PG.connect({:dbname => 'expense_organizer_test'})
 
 
 RSpec.configure do |config|
-  config.after.(:each) do
+  config.after(:each) do
     DB.exec("DELETE FROM expenses *;")
   end
 end
@@ -19,17 +19,17 @@ describe("Expense") do
   end
 
   describe("#==") do
-    it("is an expense if the description is the same") do
-      test_expense = Expense.new({:description => "coffee", :amount => "2.50", :date => 2015-01-01 })
-      test_expense2 = Expense.new({:description => "coffee", :amount => "2.50", :date => 2015-01-01 })
+    it("is an expense if the description, amount, and date are the same") do
+      test_expense = Expense.new({:description => "coffee", :amount => 250, :date => "2015-01-01" })
+      test_expense2 = Expense.new({:description => "coffee", :amount => 250, :date => "2015-01-01" })
       expect(test_expense).to(eq(test_expense2))
     end
   end
 
   describe("#save") do
     it("saves the expense to an array of expenses") do
-      test_expense = Expense.new({:description => "coffee", :amount => "2.50", :date => 2015-01-01 })
-      text_expense.save()
+      test_expense = Expense.new({:description => "coffee", :amount => 250, :date => '2015-01-01' })
+      test_expense.save()
       expect(Expense.all()).to(eq([test_expense]))
     end
   end
